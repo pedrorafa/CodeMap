10a8177e31cb857f959daff34a04f8e4fd4da880
Pedro Rafael <pedrorafag00@gmail.com>
2022-03-10 15:29:33 -0300

    update files and first component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Footer from "../components/common/footer";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -19,54 +20,19 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          <code className={styles.code}>Patterns</code>
+          <code className={styles.code}>Patterns List</code>
         </p>
 
         <div className={styles.grid}>
-          <Link href={'behavioral/strategy'}>
+          <Link href={"behavioral/strategy"}>
             <div className={styles.card}>
               <h2>Strategy</h2>
               <p>Learn this Pattern</p>
             </div>
           </Link>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 };
